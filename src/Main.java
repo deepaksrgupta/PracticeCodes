fd391f3c78dd4d6b9bff615180c4bda677a0b892
deepakgupta <deepaksrgupta@gmail.com>
2020-03-29 19:00:24 +0530

    #added max coin from balloon burst

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,7 @@ import java.util.*;
 
 public class Main {
 
+
     public static void main(String[] args) {
-        char [] c = {'a','b'};
-        String s  = new String(c);
-        System.out.println();
     }
 }
