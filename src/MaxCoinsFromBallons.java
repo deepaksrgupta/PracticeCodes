eaef0f07141bb291538508d26d5d45f074e8095d
deepakgupta <deepaksrgupta@gmail.com>
2020-04-04 18:27:28 +0530

    #added comments in bottom up for max coins from balloons pop

--- src/MaxCoinsFromBallons.java
+++ src/MaxCoinsFromBallons.java
@@ -10,12 +10,46 @@ public class MaxCoinsFromBallons {
         for (int k = 2; k < n; ++k)
             for (int left = 0; left < n - k; ++left) {
                 int right = left + k;
-                for (int i = left + 1; i < right; ++i)
+                for (int i = left + 1; i < right; ++i){
                     dp[left][right] = Math.max(dp[left][right],
                             nums[left] * nums[i] * nums[right] + dp[left][i] + dp[i][right]);
+                    //System.out.println("k="+k+" left="+left+" i="+i+" right="+right +" dp="+dp[left][right]+" nums[left]="+nums[left]+" nums[i]="+nums[i]+" nums[right]="+nums[right]);
+                }
+
             }
 
         return dp[0][n - 1];
+    }
+
+    public int burstBallonsDP(int[] a){
+
+        int dp[][] = new int[a.length+2][a.length+2];
+
+        int nums[] = new int[a.length+2];
+
+        for(int i=1;i <= a.length; i++)  nums[i] = a[i-1];
+        nums[0] = nums[nums.length-1] = 1;
+
+
+        for(int length  = 2; length < nums.length; length++){       //checks for all subarray length
+
+            for(int left = 0; left < nums.length - length; left++) { //starting of sub array
+
+                int right  = left + length;     // max sub array can go
+
+
+                for(int k = left +1; k < right; k++){   // tries all possible combination of subarrays to build up dp
+
+                    dp[left][right] = Math.max(dp[left][right], dp[left][k] + (nums[left]*nums[k]*nums[right]) +dp[k][right]);
+
+                }
+
+            }
+
+        }
+
+
+        return dp[0][nums.length-1];
     }
 
     public int maxCoins(int[] iNums) {
@@ -42,10 +76,10 @@ public class MaxCoinsFromBallons {
 
     public static void main(String[] args) {
 
-        //int[] a = {3,1,5,8};
+        //int[] a = {3,2,5,8};
         int[] a = {1, 2, 3, 4, 3};
 
-        int result = new MaxCoinsFromBallons().maxCoins(a);
+        int result = new MaxCoinsFromBallons().burstBallonsDP(a);
 
         System.out.println(result);
     }
