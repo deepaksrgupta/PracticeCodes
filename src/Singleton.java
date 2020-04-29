4472a7426957c01dc0c8de026d494067bbacb22c
deepakgupta <deepaksrgupta@gmail.com>
2020-04-29 21:16:05 +0530

    #added sync in singleton class for multi threading env

--- src/Singleton.java
+++ src/Singleton.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 
 public class Singleton {
-    private static Singleton _instance = new Singleton();
+    private static Singleton _instance;
     private static int random = new Random().nextInt(100);
 
     private Singleton(){
@@ -9,6 +9,19 @@ public class Singleton {
     }
 
     public static Singleton getSingleton(){
+
+        if (_instance == null) {
+
+            //added monitor for first 2 threads when race condition is going to happen in multithreading env
+            synchronized (Singleton.class){
+
+                if(_instance == null){
+                    _instance = new Singleton();
+                }
+            }
+
+        }
+
         return _instance;
     }
 
