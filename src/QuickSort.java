66d8b9bb3372b14cbf8467d8380aa5a971ac664d
deepaksrgupta <deepaksrgupta@gmail.com>
2020-11-13 14:07:48 +0530

    #added randomized support in quick sort

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,4 +1,15 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class QuickSort {
+
+    private boolean isRandomized  = false;
+    private final Random random = new Random();
+
+    public void setRandomized(boolean isRandomized){
+        this.isRandomized = isRandomized;
+    }
+
     public void quickSort(int a[], int low, int high){
 
         if(high>low){
@@ -18,6 +29,13 @@ public class QuickSort {
         //choose last element as pivot
         int pivot  = a[end];
 
+        if(isRandomized){
+            int randomIndex  = getRandomInteger(end - start) + start;
+            pivot  = a[randomIndex];
+            swap(a, randomIndex, end);
+        }
+
+        //keep putting all the less elements in start
         for(int i = start; i < end; i++){
 
             if(a[i] < pivot) {
@@ -36,5 +54,24 @@ public class QuickSort {
         int temp = a[i];
         a[i] = a[j];
         a[j] = temp;
+    }
+
+    private int getRandomInteger(int range){
+        return random.nextInt(range);
+    }
+
+    public static void main(String args[]){
+
+        QuickSort quickSort = new QuickSort();
+        quickSort.setRandomized(true);
+
+        int[] arr = new int[quickSort.getRandomInteger(100)];
+
+        for(int i=0;i< arr.length; i++){
+            arr[i] = quickSort.getRandomInteger(100);
+        }
+
+        quickSort.quickSort(arr,0 , arr.length-1);
+        System.out.println(Arrays.toString(arr));
     }
 }
