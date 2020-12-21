818eba9d4d5448bd3cf1711e45c01f79459c9d09
deepaksrgupta <deepaksrgupta@gmail.com>
2020-12-21 11:10:03 +0530

    #added number of bst (catalans number)

---
+++ src/NumOfBST.java
@@ -0,0 +1,24 @@
+public class NumOfBST {
+    /*
+    https://leetcode.com/problems/unique-binary-search-trees/submissions/
+    Runtime: 0 ms, faster than 100.00% of Java online submissions for Unique Binary Search Trees.
+    Memory Usage: 35.9 MB, less than 34.04% of Java online submissions for Unique Binary Search Trees.
+
+    catalans number -> 2nCn / n + 2 -> can use direct formula for linear time solution
+     */
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+
+        return dp[n];
+    }
+}
+
+
