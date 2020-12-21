96be948a3933671a9a16cda83f509baf8db25a24
deepaksrgupta <deepaksrgupta@gmail.com>
2020-12-21 11:05:47 +0530

    #changed #LongestPalindromeSubstring implementation

--- src/LongestPalindromeSubstring.java
+++ src/LongestPalindromeSubstring.java
@@ -1,17 +1,35 @@
 public class LongestPalindromeSubstring {
-    public static int solveLPS(char[] q,int start,int end){
-        if(start == end){
-            return 1;
-        }
 
-        if(q[start] == q[end] && start+1 == end) {
-            return 2;
-        }
+    /*
+    https://leetcode.com/problems/longest-palindromic-substring/submissions/
+    Runtime: 24 ms, faster than 80.97% of Java online submissions for Longest Palindromic Substring.
+    Memory Usage: 39.3 MB, less than 63.21% of Java online submissions for Longest Palindromic Substring.
+     */
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
 
-        if(q[start] == q[end]){
-            return 2 + solveLPS(q,start+1,end-1);
-        }
+        for (int i = 0; i < s.length(); i++) {
 
-        return Math.max(solveLPS(q,start+1,end),solveLPS(q,start,end-1));
+            int len1 = expandAroundCenter(s, i, i);
+
+            int len2 = expandAroundCenter(s, i, i + 1);
+
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
     }
 }
